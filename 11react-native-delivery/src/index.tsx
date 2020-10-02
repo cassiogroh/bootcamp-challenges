ec712956c93a56a3da15193a86c757ef185259af
cassiogroh <cassio_groh@hotmail.com>
2020-10-01 23:00:31 -0300

    Challenge accepted

--- 11react-native-delivery/src/index.tsx
+++ 11react-native-delivery/src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 
 import Routes from './routes';
+import { ServerContainer } from '@react-navigation/native';
 
 const App: React.FC = () => (
   <View style={{ flex: 1 }}>
@@ -16,3 +17,6 @@ const App: React.FC = () => (
 );
 
 export default App;
+
+// yarn json-server server.json -p 3333
+// adb reverse tcp:3333 tcp:3333
